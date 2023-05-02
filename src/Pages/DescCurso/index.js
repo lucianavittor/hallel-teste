9cb8586f84a762d913c9cbbcb19661ca11a9a8e5
Luciana Vitor <2021331241@ifam.edu.br>
2023-05-02 15:51:42 -0400

    Adicionando projeto

---
+++ src/Pages/DescCurso/index.js
@@ -0,0 +1,16 @@
+
+function DescCurso() {
+    return(
+
+        <div className="left">
+            <div className="conteudo-topo">
+                
+
+            </div>
+
+        </div>
+    );
+    
+}
+
+export default DescCurso;
